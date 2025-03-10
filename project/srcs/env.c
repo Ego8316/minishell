5f85aca0dcb24f0d6e628d5f49f9414a182b3665
Ego8316 <ego@PCE>
2025-03-10 19:50:46 +0100

    added env.c

---
+++ project/srcs/env.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 18:32:59 by ego               #+#    #+#             */
+/*   Updated: 2025/03/10 18:37:58 by ego              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	*free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		if (arr[i])
+			free(arr[i]);
+		i++;
+	}
+	if (arr)
+		free(arr);
+	return (NULL);
+}
+
+char	**copy_envp(char **envp)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	copy = (char **)malloc(i * sizeof(char *));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		copy[i] = ft_strdup(envp[i]);
+		if (!copy[i])
+			return ((char **)free_array(copy));
+		i++;
+	}
+	return (copy);
+}
