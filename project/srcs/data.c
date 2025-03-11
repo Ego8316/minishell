d458ebe565f4800b2e81cab02ada344e12987953
Ego8316 <ego@PCE>
2025-03-11 13:28:10 +0100

    moved copy_envp to data.c and created builtins folder. Updated Makefile accordingly

--- project/srcs/data.c
+++ project/srcs/data.c
@@ -6,11 +6,63 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 12:51:34 by ego               #+#    #+#             */
-/*   Updated: 2025/03/11 13:06:38 by ego              ###   ########.fr       */
+/*   Updated: 2025/03/11 13:27:40 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief Frees all there is to free in a NULL-terminated
+ * array of strings and returns NULL.
+ * 
+ * @return NULL.
+ */
+void	*free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	if (arr)
+		free(arr);
+	return (NULL);
+}
+
+/**
+ * @brief Copies the envp variable and returns the
+ * allocated copy.
+ * 
+ * @param envp Environment to be copied.
+ * 
+ * @return The allocated copy, NULL if allocation fails.
+ */
+char	**copy_envp(char **envp)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	copy = (char **)malloc((i + 1) * sizeof(char *));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		copy[i] = ft_strdup(envp[i]);
+		if (!copy[i])
+			return ((char **)free_array(copy));
+		i++;
+	}
+	copy[i] = NULL;
+	return (copy);
+}
 
 /**
  * @brief Initializes the data structure.
