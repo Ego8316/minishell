d162e12d7f22f6802f1cc1046f0a0151e774730d
Ego8316 <ego@PCE>
2025-03-12 15:41:24 +0100

    tested a few cases with echo, seem to work fine

--- project/srcs/main.c
+++ project/srcs/main.c
@@ -6,22 +6,45 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 15:28:01 by pkurt             #+#    #+#             */
-/*   Updated: 2025/03/11 18:14:41 by ego              ###   ########.fr       */
+/*   Updated: 2025/03/12 15:40:04 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_token	*test_token(char *s1, char *s2, char *s3)
+{
+	t_token	*arg1;
+	t_token	*arg2;
+	t_token	*arg3;
+
+	arg1 = malloc(sizeof(t_token));
+	arg2 = malloc(sizeof(t_token));
+	arg3 = malloc(sizeof(t_token));
+	arg1->str = ft_strdup(s1);
+	arg2->str = ft_strdup(s2);
+	arg3->str = ft_strdup(s3);
+	arg1->type = TEXT;
+	arg2->type = TEXT;
+	arg3->type = TEXT;
+	arg1->nxt = arg2;
+	arg2->nxt = arg3;
+	arg3->nxt = 0;
+	return (arg1);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
+	t_token	*test;
 
 	(void)argc;
 	argv[0] = 0;
 	if (!data_init(&data, envp))
 		return (1);
-	env_builtin(&data, 0);
-	pwd_builtin(&data, 0);
-	// while (1)
-	run_cmd_from_user();
+	test = test_token("-nnnn3", "salut", "-n");
+	echo_builtin(test);
+	printf("\n");
+	env_builtin(&data, test);
+	// run_cmd_from_user();
 }
