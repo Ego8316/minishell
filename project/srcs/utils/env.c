4942492dc86d9dc885f5ac86d85995b3674bcbf1
Ego8316 <ego@PCE>
2025-03-12 16:54:28 +0100

    get value written have to test it

--- project/srcs/utils/env.c
+++ project/srcs/utils/env.c
@@ -6,7 +6,7 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:22:07 by ego               #+#    #+#             */
-/*   Updated: 2025/03/12 16:28:36 by ego              ###   ########.fr       */
+/*   Updated: 2025/03/12 16:50:10 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,32 @@ int	add_line(t_data *data, char *line)
 	free(data->envp);
 	data->envp = envp;
 	return (0);
+}
+
+/**
+ * @brief Goes through the environment variable and searches
+ * for the value corresponding to the given key. Allocates
+ * memory to give a copy of the value if found, empty string
+ * otherwise.
+ * 
+ * @param data Pointer to the data structure.
+ * @param key Key whose value is to be found.
+ * 
+ * @return Allocated copy of the value if found, allocated empty
+ * string ("\0") otherwise. NULL if allocation fails.
+ */
+char	*get_value(t_data *data, char *key)
+{
+	int	len;
+	int	i;
+
+	len = ft_strlen(key);
+	i = 0;
+	while (data->envp[i])
+	{
+		if (!ft_strncmp(key, data->envp[i], len))
+			return (ft_strdup(data->envp[i] + len + 1));
+		i++;
+	}
+	return (ft_strdup(""));
 }
