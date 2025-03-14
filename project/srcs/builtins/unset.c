b266c2c800505c6aaaff5cb93f649274288a671e
Ego8316 <ego@PCE>
2025-03-14 14:48:05 +0100

    unset seems to work with edge cases!

--- project/srcs/builtins/unset.c
+++ project/srcs/builtins/unset.c
@@ -6,11 +6,47 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:36:40 by ego               #+#    #+#             */
-/*   Updated: 2025/03/13 03:08:21 by ego              ###   ########.fr       */
+/*   Updated: 2025/03/14 14:40:47 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief Goes through the var list to delete the node
+ * with identifier identical to the argument given.
+ * Does nothing if no such node can be found.
+ * 
+ * @param data Pointer to the data structure.
+ * @param arg Supposedly the identifier.
+ */
+static void	unset_arg(t_data *data, char *arg)
+{
+	t_var	*v;
+	t_var	*prev;
+	t_var	*tmp;
+
+	v = data->vars;
+	prev = NULL;
+	while (v)
+	{
+		if (!ft_strcmp(v->identifier, arg))
+		{
+			if (!prev)
+				data->vars = v->nxt;
+			else
+				prev->nxt = v->nxt;
+			tmp = v;
+			v = v->nxt;
+			free_str(&tmp->identifier);
+			free_str(&tmp->value);
+			free(tmp);
+			return ;
+		}
+		prev = v;
+		v = v->nxt;
+	}
+}
 
 /**
  * @brief Executes the unset builtin: removes
@@ -23,25 +59,9 @@
  */
 int	unset_builtin(t_data *data, t_token *args)
 {
-	t_var	*v;
-
-	if (!args)
-		return (0);
 	while (args && args->type == TEXT)
 	{
-		v = data->vars;
-		while (v)
-		{
-			if (!ft_strcmp(v->identifier, args->str))
-			{
-				if (v->type == ENV)
-					return (0); // Here should remove from envp
-				free(v->value);
-				v->value = ft_strdup("");
-				v->type = LOCAL;
-			}
-			v = v->nxt;
-		}
+		unset_arg(data, args->str);
 		args = args->nxt;
 	}
 	return (0);
